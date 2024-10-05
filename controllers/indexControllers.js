4ff7c3474b429c28e6c4a72f492cc5ecbbf5b08b
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-10-05 14:21:34 +0200

    Correct bug with user and member

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -2,7 +2,7 @@ const db = require("../db/queries");
 
 const getMessages = async (req, res) => {
   const messages = await db.getMessages();
-  const user = await db.getUserById(req.user.id);
+  const user = req.user ? await db.getUserById(req.user.id) : [null];
   const isMember = user[0] ? user[0].member : false;
   const isAdmin = user[0] ? user[0].admin : false;
   res.render("index", {
