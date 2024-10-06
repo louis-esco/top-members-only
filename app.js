3843c97c92e7ca3d4e399d17d87a312f46852a89
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-10-06 18:08:13 +0200

    Remove unused code

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
 app.use(
   session({
     store: sessionStore,
